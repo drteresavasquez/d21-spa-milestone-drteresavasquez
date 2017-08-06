429b57e7a4885076cf60d7d9fb3752afd49b4692
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-08-06 13:02:23 -0500

    updates

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,9 @@
 console.log("main.js, YO!");
 
 {
-
- let loadCars = new XMLHttpRequest();
- loadCars.open("GET", "inventory.json");
-loadCars.send();
+	let loadCars = new XMLHttpRequest();
+	loadCars.open("GET", "inventory.json");
+	loadCars.send();
 
 	loadCars.addEventListener("load", DataComplete);
 	loadCars.addEventListener("error", DataFailed);
@@ -26,18 +25,25 @@ var carListings = {};
 	// console.log("carListings", carListings);
 	cars = Object.keys(carListings.cars);
 	
-	cars.forEach(function(id){
+		cars.forEach(function(id){
 		// console.log(carListings.cars[id].make);
-		carsDiv.innerHTML += `<div> <li>${carListings.cars[id].make} | ${carListings.cars[id].model} | ${carListings.cars[id].year} | ${carListings.cars[id].price} | ${carListings.cars[id].description}</li></div>`
-	})
+		carsDiv.innerHTML += `<div class="card col-12 col-sm-4">
+		<div class="card-header"><h4>${carListings.cars[id].year} ${carListings.cars[id].make} ${carListings.cars[id].model} </h4></div>
+			<img class="card-img-top" src="${carListings.cars[id].image}" alt="Card image cap">
+				<div class="card-block">
+					<p class="card-text">${carListings.cars[id].description}</p>
+					<p>Price: $${carListings.cars[id].price}</p>
+					  <a href="#" class="btn btn-primary">Learn More</a>
+				</div>
+			</div>
+		`
+	});
 
-}
+};
 
 function DataFailed(event){
 	console.log("oops the data failed");
 };
-
-
 
 
 }(Carlot || {});
